593af2778a6f44182172fa2f5017f36f4b9dc175
sarafkushagra <ksaraf0004@gmail.com>
2025-08-03 18:25:22 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useMemo } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Footer from './Components/Footer';
+import Footer from './components/Footer';
 import ItemListPage from './components/ItemsListPage';
 import Details from './components/Details';
 import CartPage from './components/CartPage';
